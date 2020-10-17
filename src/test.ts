f265217835a093a14f7bc01347dfd8c8e7124711
Alexandru Jercan <jercan_alex27@yahoo.com>
2020-10-17 18:43:22 +0300

    Added Tests

---
+++ src/test.ts
@@ -0,0 +1,33 @@
+import * as jwtWrapper from './index';
+
+describe('Unit Tests', () => {
+  describe('Pool Tests', () => {
+    it('should work when setup is correct', () => {
+      const user = { username: 'username' };
+      const secret = 'secret';
+
+      const token = jwtWrapper.sign(user, secret);
+      const verified = jwtWrapper.verify(token, secret);
+
+      expect(verified).toBeDefined();
+      expect(verified?.username).toStrictEqual(user.username);
+    });
+    it('should not work when secret is incorrect', () => {
+      const user = { username: 'username' };
+      const secret = 'secret';
+      const otherSecret = 'secret1';
+
+      const token = jwtWrapper.sign(user, secret);
+      const verified = jwtWrapper.verify(token, otherSecret);
+
+      expect(verified).toBeUndefined();
+    });
+    it('should not work when setup is incorrect', () => {
+      const secret = 'secret';
+
+      const verified = jwtWrapper.verify(undefined, secret);
+
+      expect(verified).toBeUndefined();
+    });
+  });
+});
